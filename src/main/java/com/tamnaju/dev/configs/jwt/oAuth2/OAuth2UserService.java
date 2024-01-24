d109ca37c7308c18758de9c8160d57f723443f39
daniel <shuffler54@hotmail.com>
2024-01-25 04:19:12 +0900

    2024-01-25

--- src/main/java/com/tamnaju/dev/configs/jwt/oAuth2/OAuth2UserService.java
+++ src/main/java/com/tamnaju/dev/configs/jwt/oAuth2/OAuth2UserService.java
@@ -34,20 +34,22 @@ public class OAuth2UserService extends DefaultOAuth2UserService {
     public OAuth2User loadUser(OAuth2UserRequest userRequest) throws OAuth2AuthenticationException {
         OAuth2User oAuth2User = super.loadUser(userRequest);
 
-        log.info("OAuth2UserService loadUser() oAuth2User :\n" + oAuth2User.toString());
+        log.info("OAuth2UserService loadUser() oAuth2User :\n"
+                + oAuth2User.toString());
 
         Map<String, Object> attributes;
-        String email = "";
+        String email;
         LocalDate birth;
         String provider = userRequest.getClientRegistration().getRegistrationId();
         String providerId;
+
+        log.info("OAuth2UserService loadUser() oAuth2User.getAttributes() :\n"
+                + oAuth2User.getAttributes());
 
         // provider에 따른 조건 분기
         switch (provider) {
             case "kakao":
                 attributes = (Map<String, Object>) oAuth2User.getAttributes().get("kakao_account");
-                log.info("OAuth2UserService loadUser() attributes :\n"
-                        + attributes);
                 email = attributes.get("email").toString();
                 birth = LocalDate.parse(attributes.get("birthyear").toString()
                         + "-" + attributes.get("birthday").toString()
@@ -56,23 +58,31 @@ public class OAuth2UserService extends DefaultOAuth2UserService {
                                 .substring(2));
                 providerId = provider + "_" + oAuth2User.getAttributes().get("id").toString();
                 break;
+
             case "naver":
                 attributes = (Map<String, Object>) oAuth2User.getAttributes().get("response");
+                email = "";
                 birth = LocalDate.now();
                 providerId = attributes.get("id").toString();
                 break;
+
             case "google":
                 attributes = oAuth2User.getAttributes();
+                email = "";
                 birth = LocalDate.now();
                 providerId = attributes.get("sub").toString();
                 break;
+
             default:
                 throw new OAuth2AuthenticationException("Unexpected provider");
         }
 
-        log.info("OAuth2UserService loadUser() email :\n" + email);
-        log.info("OAuth2UserService loadUser() birth :\n" + birth);
-        log.info("OAuth2UserService loadUser() providerId :\n" + providerId);
+        log.info("OAuth2UserService loadUser() email :\n"
+                + email);
+        log.info("OAuth2UserService loadUser() birth :\n"
+                + birth);
+        log.info("OAuth2UserService loadUser() providerId :\n"
+                + providerId);
 
         // DB에 user 정보 여부에 따른 처리
         UserDto userDto;
@@ -81,7 +91,7 @@ public class OAuth2UserService extends DefaultOAuth2UserService {
             UserEntity userEntity = UserEntity.builder()
                     .id(providerId)
                     .email(email)
-                    .password(passwordEncoder.encode(oAuth2User.hashCode() + "").substring(0,60))
+                    .password(passwordEncoder.encode(oAuth2User.hashCode() + "").substring(0, 60))
                     .birth(birth)
                     .provider(provider)
                     .providerId(providerId)
@@ -89,11 +99,13 @@ public class OAuth2UserService extends DefaultOAuth2UserService {
             userMapper.saveUser(userEntity);
             // DB에 없을 시, 새로 생성한 User 정보 할당
             userDto = UserDto.userEntityToUserDto(userEntity);
-            log.info("OAuth2UserService loadUser() 신규 회원 : " + providerId);
+
+            log.info("OAuth2UserService loadUser() new member : " + providerId);
         } else {
             // DB에 있을 시, 기존 User 정보 할당
             userDto = UserDto.userEntityToUserDto(dbUserEntity);
-            log.info("OAuth2UserService loadUser() 기존 회원 : " + providerId);
+
+            log.info("OAuth2UserService loadUser() old member : " + providerId);
         }
 
         PrincipalDetails oAuth2UserInfo = PrincipalDetails.builder()
