63e5fc5b63f47b4f67d621eb515207d55502c4f2
tonyking11 <tonysung1@naver.com>
2024-01-31 20:32:04 +0900

    weather-icon-ok

--- src/main/resources/static/scripts/jejumap/weather.js
+++ src/main/resources/static/scripts/jejumap/weather.js
@@ -5,33 +5,6 @@ const option = {
     level: 8 //확대, 축소 레벨
 };
 map.instance = new kakao.maps.Map(map, option);
-console.log(kakao.maps.event);
-console.log(kakao.maps.event.addListener);
-
-kakao.maps.event.addListener(map,'click', function (mouseEvent) {
-    console.log(mouseEvent);
-    console.log(typeof mouseEvent);
-    let clickedLatLng = mouseEvent.latLng;
-    console.log('hello', clickedLatLng);
-    console.log('Clicked Longitude:', clickedLatLng.getLng());
-    console.log('Clicked Latitude:', clickedLatLng.getLat());
-
-    let coords = new kakao.maps.LatLng(clickedLatLng.getLat(), clickedLatLng.getLng());
-    let wgs84Coords = kakao.maps.CoordType.UTMK.fromLatLngToWGS84(coords);
-
-    getWeatherInfo(wgs84Coords.getLat(), wgs84Coords.getLng());
-});
-
-function getWeatherInfo(latitude, longitude) {
-
-    axios.get(`https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=47eccf027772f4e4e0e2fe8a3cb91e64`)
-        .then(res =>{
-            console.log("잘 작동 된다.");
-        })
-        .catch(err=>{console.log(err);});
-
-}
-
 
 const cover = document.getElementById('cover');
 
@@ -41,19 +14,60 @@ cover.isVisible = () => cover.classList.contains('visible');
 
 const info = document.getElementById('info');
 
+
+function getWeatherInfo(latitude, longitude) {
+    console.log("function 안으로 들어옴");
+    axios.get(`https://api.openweathermap.org/data/2.5/weather?lat=`+ latitude +`&lon=`+ longitude +`&appid=47eccf027772f4e4e0e2fe8a3cb91e64`)
+        .then(res =>{
+            const responseObject = res.data;
+            console.log(res.data.weather);
+            console.log(res.data.main);
+            // const items = responseObject['weather'];
+            console.log(responseObject);
+
+                info.show(res.data.weather[0],res.data.main);
+
+
+
+
+            console.log("잘 작동 된다.");
+
+
+        })
+        .catch(err=>{console.log(err);});
+
+}
+
 info.querySelector('[rel="close"]').onclick = function () {
     cover.hide();
     info.hide();
 }
 
-info.show = (item) => {
-    info.querySelector('[rel="temperature"]').innerText = item['introduction'];
-    info.querySelector('[rel="sky"]').innerText = item['phoneno'];
-    info.querySelector('[rel="rain-probability"]').innerText = item[''];
-    info.querySelector('[rel="rain-style"]').innerText = item[''];
-
+info.show = (weather,main) => {
+    info.querySelector('[rel="icon"]').innerHTML = `<img src="https://openweathermap.org/img/wn/${weather['icon']}@2x.png" alt="weather_icon">`;
+    console.log('item',weather);
+    info.querySelector('[rel="temperature"]').innerText = Math.floor((main['temp'] - 273.15)*100)/100;
+    info.querySelector('[rel="sky"]').innerText = weather['main'];
+    info.querySelector('[rel="rain-style"]').innerText = weather['id'];
+    // info.querySelector('[rel="rain-probability"]').innerText = item['강수확률'];
+    info.querySelector('[rel="description"]').innerText = weather['description'];
 
     info.classList.add('visible');
 };
+
 info.hide = () => info.classList.remove('visible');
 info.isVisible = () => info.classList.contains('visible');
+
+
+
+kakao.maps.event.addListener(map.instance,'click', function (position) {
+    console.log(position);
+    console.log(typeof position);
+    let clickedLatLng = position.latLng;
+    console.log('hello', clickedLatLng);
+    console.log('Clicked Longitude:', clickedLatLng.getLng());
+    console.log('Clicked Latitude:', clickedLatLng.getLat());
+    console.log(kakao.maps.services.Coords);
+
+getWeatherInfo(clickedLatLng.getLat(), clickedLatLng.getLng());
+});
